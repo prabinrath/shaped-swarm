9d3155aaa856f26199a88ca03aa48c1607d2f722
Prabin Rath <prabinrath123@gmail.com>
2023-11-30 01:28:02 -0700

    collision avoidance working

---
+++ barrier_certificate.py
@@ -0,0 +1,53 @@
+import numpy as np
+from cvxopt.solvers import qp, options
+from cvxopt import matrix, sparse
+from scipy.special import comb
+
+def barrier_certificate(dxi, x, barrier_gain=100, safety_radius=0.10, magnitude_limit=0.25):
+    """
+    barrier_gain: double (controls how quickly agents can approach each other.  lower = slower)
+    safety_radius: double (how far apart the agents will stay)
+    magnitude_limit: how fast the robot can move linearly.
+
+    -> function (the barrier certificate function)
+    source https://github.com/robotarium/robotarium_python_simulator/blob/master/rps/utilities/barrier_certificates.py
+    """
+
+    #Check user input types
+    assert isinstance(dxi, np.ndarray), "In the function created by the create_single_integrator_barrier_certificate function, the single-integrator robot velocity command (dxi) must be a numpy array. Recieved type %r." % type(dxi).__name__
+    assert isinstance(x, np.ndarray), "In the function created by the create_single_integrator_barrier_certificate function, the robot states (x) must be a numpy array. Recieved type %r." % type(x).__name__
+
+    #Check user input ranges/sizes
+    assert x.shape[0] == 2, "In the function created by the create_single_integrator_barrier_certificate function, the dimension of the single integrator robot states (x) must be 2 ([x;y]). Recieved dimension %r." % x.shape[0]
+    assert dxi.shape[0] == 2, "In the function created by the create_single_integrator_barrier_certificate function, the dimension of the robot single integrator velocity command (dxi) must be 2 ([x_dot;y_dot]). Recieved dimension %r." % dxi.shape[0]
+    assert x.shape[1] == dxi.shape[1], "In the function created by the create_single_integrator_barrier_certificate function, the number of robot states (x) must be equal to the number of robot single integrator velocity commands (dxi). Recieved a current robot pose input array (x) of size %r x %r and single integrator velocity array (dxi) of size %r x %r." % (x.shape[0], x.shape[1], dxi.shape[0], dxi.shape[1])
+
+    
+    # Initialize some variables for computational savings
+    N = dxi.shape[1]
+    num_constraints = int(comb(N, 2))
+    A = np.zeros((num_constraints, 2*N))
+    b = np.zeros(num_constraints)
+    H = sparse(matrix(2*np.identity(2*N)))
+
+    count = 0
+    for i in range(N-1):
+        for j in range(i+1, N):
+            error = x[:, i] - x[:, j]
+            h = (error[0]*error[0] + error[1]*error[1]) - np.power(safety_radius, 2)
+
+            A[count, (2*i, (2*i+1))] = -2*error
+            A[count, (2*j, (2*j+1))] = 2*error
+            b[count] = barrier_gain*np.power(h, 3)
+
+            count += 1
+
+    # Threshold control inputs before QP
+    norms = np.linalg.norm(dxi, 2, 0)
+    idxs_to_normalize = (norms > magnitude_limit)
+    dxi[:, idxs_to_normalize] *= magnitude_limit/norms[idxs_to_normalize]
+
+    f = -2*np.reshape(dxi, 2*N, order='F')
+    result = qp(H, matrix(f), matrix(A), matrix(b))['x']
+
+    return np.reshape(result, (2, -1), order='F')
--- shaped_swarm_run.py
+++ shaped_swarm_run.py
@@ -1,6 +1,10 @@
 from shaped_swarm_env import ShapedSwarmEnv
 import numpy as np
 from img_sdf import ImageSdf
+from barrier_certificate import barrier_certificate
+
+# TODO: reduce text width in in image
+# TODO: implement cross product control law for bug motion
 
 def main():
     imgsdf = ImageSdf('art/U.png', range=(1.5, 1.5))
@@ -9,7 +13,7 @@ def main():
         n_bots=NUM_ROBOTS,
     )
     with ShapedSwarmEnv(env_config) as env:
-        SIM_TIME_SEC = 100
+        SIM_TIME_SEC = 40
         SIM_STEP = 50 * 1e-3
         count = 0
         target_vels = np.zeros((NUM_ROBOTS, 2))
@@ -19,6 +23,7 @@ def main():
                 gx, gy = imgsdf.get_gradient(curr_state[i][0], curr_state[i][1])
                 target_vels[i,0] = gx
                 target_vels[i,1] = gy
+            target_vels = barrier_certificate(target_vels.T, curr_state[:,:2].T).T
             count+=1
 
 main()
