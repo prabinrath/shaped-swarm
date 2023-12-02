f2a8aa9c6af0d99d1390cfec73a64c076010ef14
Prabin Rath <prabinrath123@gmail.com>
2023-12-02 15:41:36 -0700

    Update shaped_swarm_run.py

--- shaped_swarm_run.py
+++ shaped_swarm_run.py
@@ -4,7 +4,7 @@ from img_sdf import ImageSdf
 from barrier_certificate import barrier_certificate
 
 def main():
-    NUM_ROBOTS = 12
+    NUM_ROBOTS = 16
     env_config = dict(
         n_bots=NUM_ROBOTS,
     )
@@ -12,7 +12,7 @@ def main():
         SIM_STEP = 50 * 1e-3
         text = 'A*S*U'
         for char in text:
-            imgsdf = ImageSdf(f'art/{char}.png', range=(1.5, 1.5))
+            imgsdf = ImageSdf(f'art/{char}.png', range=(2.0, 2.0))
             count = 0
             target_vels = np.zeros((NUM_ROBOTS, 2))
             SIM_TIME_SEC = 20 if char=='*' else 60
