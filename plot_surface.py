2a8189654b8266059137bddb4ee16a757ba4ef92
Prabin Rath <prabinrath123@gmail.com>
2023-12-08 00:53:13 -0700

    Update plot_surface.py

--- plot_surface.py
+++ plot_surface.py
@@ -3,12 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import cv2
 
-# Since I don't have the actual image, I will create a dummy binary image with shape (400, 400)
-# In a real scenario, you would load the image using a library like PIL or OpenCV and then convert it to grayscale
-# For example, using PIL: image = Image.open('path_to_image.png').convert('L')
-# Then convert the image to a numpy array: image_array = np.array(image)
-
-image_array = cv2.imread('/home/prabin/Pictures/U_sdf.png', cv2.IMREAD_GRAYSCALE)
+image_array = cv2.imread('results/U_sdf.png', cv2.IMREAD_GRAYSCALE)
 
 # Normalize the pixel values to the range [0, 1]
 normalized_image = image_array / 255.0
